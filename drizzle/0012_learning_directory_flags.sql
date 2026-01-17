f1547c02daab52e2f09f06b2834872114e7ea928
Jack Arthur Harrhy <me@jackharrhy.com>
2026-01-17 18:47:52 -0330

    fix: add statement breakpoint to learning flags migration

--- drizzle/0012_learning_directory_flags.sql
+++ drizzle/0012_learning_directory_flags.sql
@@ -1,3 +1,4 @@
 -- Add technl and genesis flags to learning table (same as companies)
 ALTER TABLE learning ADD COLUMN technl INTEGER DEFAULT 0;
+--> statement-breakpoint
 ALTER TABLE learning ADD COLUMN genesis INTEGER DEFAULT 0;
